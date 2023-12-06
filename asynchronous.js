3870274691a0d9fb736c2c2424cd90c90f542cda
FrasIrfan <leofras0051@gmail.com>
2023-12-06 12:24:19 +0500

    used two settimeout functions

--- asynchronous.js
+++ asynchronous.js
@@ -3,4 +3,8 @@ setTimeout(function() {
     console.log('setTimeout of 5 Seconds!');
     }, 5000);
 
+setTimeout(function() {
+    console.log('setTimeout of 2 Seconds!');
+    }, 2000);
+
 console.log('Ending the program');
