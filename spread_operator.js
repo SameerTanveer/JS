bf846e1e496c8ce3d6e2258aa996c9272fc5d009
FrasIrfan <leofras0051@gmail.com>
2023-12-12 09:03:15 +0500

    spread operator in js

---
+++ spread_operator.js
@@ -0,0 +1,19 @@
+arr = [1,2,3]
+
+// function sum(a,b,c){
+//     return a+b+c
+// }
+
+// let a = [...arr, 4, 5, 99]
+
+// console.log(sum(...arr)) 
+
+obj1 = {
+    name: 'Leo',
+    age: '19',
+    favlang: 'Python'
+}
+
+let obj2 = {...obj1, favlang: 'Javascript'}   
+
+console.log(obj2)
