2f055bb32b6d52968de114ba6c00dedb5342d4fa
FrasIrfan <leofras0051@gmail.com>
2023-12-23 01:03:10 +0500

    closure is formed when a function is defined inside another function and has acces to variables from outer function

---
+++ closures.js
@@ -0,0 +1,19 @@
+function outerFunction() {
+  let outerVariable = "I am from the outer function";
+
+  function innerFunction() {
+    console.log("Executing Inner Function...")
+    console.log(outerVariable);
+  }
+
+  return innerFunction;
+}
+
+let closure = outerFunction();
+closure();
+
+
+
+//closure is formed when a function is defined inside another function 
+//and has access to variables from the outer (enclosing) function's scope, 
+//even after the outer function has finished executing.
